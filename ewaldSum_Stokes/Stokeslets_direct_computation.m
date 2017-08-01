16d1c1168e2b1958060f54404c2cd4a1d9721b4a
anupvkanale <anupvkanale@gmail.com>
2017-08-01 15:32:39 -0700

    minor changes

--- ewaldSum_Stokes/Stokeslets_direct_computation.m
+++ ewaldSum_Stokes/Stokeslets_direct_computation.m
@@ -1,18 +1,18 @@
-clear; close all;
+%% Program to simulate flow past a stokeslet using direct computation
+clear; %close all;
 
 eta = 1;
-% N = 2; % number of stokeslets
 
 % grid points
-res = 7;
+res = 13;
 nX = res;
 nY = res;
 nZ = res;
 nPoints = nX*nY*nZ;
 lim = 1;
-rx = linspace(-lim,lim,nX);
-ry = linspace(-lim,lim,nY);
-rz = linspace(-lim,lim,nZ);
+rx = linspace(0,lim,nX);
+ry = linspace(0,lim,nY);
+rz = linspace(0,lim*1000,nZ);
 rVec = zeros(3,nPoints);
 for kk=1:nZ
     for ii=1:nX
@@ -23,7 +23,36 @@ for kk=1:nZ
     end
 end
 
+% assign point force location
+nStokes = 2; % number of stokeslets
+fVec = zeros(3,nPoints);
+offset = 5;
+fLoc = [(nPoints+1)/2 + offset, (nPoints+1)/2 - offset];
+fVec(:,fLoc(1)) = [0.1; 0; 0];
+fVec(:,fLoc(2)) = [-0.1; 0; 0];
 
+%% compute flowfield
+velVec = zeros(3,nPoints);
+for ii=1:nPoints
+    rVecI = rVec(:,ii);
+    sum = 0;
+    for jj=1:nStokes
+        rVecJ = rVec(:,fLoc(jj));
+        rij = rVecI - rVecJ;
+        
+        hiMat = calcOseenTensor(rij,eta);
+        sum = sum + hiMat *fVec(:,fLoc(jj));
+    end
+    velVec(:,ii) = sum;
+end
+
+%% Post-processing
+save('direct_Stokes_data');
+plotGen("direct");
+
+
+%% Verification
+%-----------------
 % % 3D plot to check if point numbers have been assigned correctly
 % for kk=1:nZ
 %     for ii=1:nX
@@ -47,65 +76,3 @@ end
 %     jj = rem(pointNum-1,nX) + 1;
 %     ii = floor((pointNum-(kk-1)*nX*nY-1)/nX) + 1;
 % end
-% 
-% 
-% 
-% point force location
-fVec = zeros(3,nPoints);
-fLoc = [(nPoints+1)/2 + 1, (nPoints+1)/2 - 1];
-
-fVec(:,fLoc(1)) = [1; 0; 0];
-fVec(:,fLoc(2)) = [-1; 0; 0];
-
-% Compute flowfield
-velVec = zeros(3,nPoints);
-for ii=1:nPoints
-    rVecI = rVec(:,ii);
-    sum = 0;
-    for jj=1:nPoints
-        rVecJ = rVec(:,jj);
-        rij = rVecI - rVecJ;
-        
-        if norm(rij)~=0
-            hiMat = calcOseenTensor(rij,eta);
-            sum = sum + hiMat *fVec(:,jj);
-        end
-    end
-    velVec(:,ii) = sum;
-end
-
-%% Plotting
-%----------------
-scale = 500;
-set(figure, 'Position', [2700, 1000, 1000, 700]);
-title('Streamlines for slow past a Stokeslet');
-
-[X,Y,Z] = sphere(4);
-for ii=1:length(fLoc)
-    scatter3(rVec(1,fLoc(ii)), rVec(2,fLoc(ii)), rVec(3,fLoc(ii)), 'filled');
-    hold on;
-end
-xlim([-1 1]); ylim([-1 1]); zlim([-1 1])
-
-% Convert to format appropriate for plotting
-u3d = zeros(nX,nY,nZ);
-v3d = zeros(nX,nY,nZ);
-w3d = zeros(nX,nY,nZ);
-for pointNum=1:nPoints
-    kk = floor((pointNum-1)/(nX*nY)) + 1;
-    jj = rem(pointNum-1,nX) + 1;
-    ii = floor((pointNum-(kk-1)*nX*nY-1)/nX) + 1;
-    u3d(ii,jj,kk) = velVec(1,pointNum);
-    v3d(ii,jj,kk) = velVec(2,pointNum);
-    w3d(ii,jj,kk) = velVec(3,pointNum);
-end
-
-% Plot flowfield in 2D
-u2d = squeeze(u3d(:,:,(nZ+1)/2));
-v2d = squeeze(v3d(:,:,(nZ+1)/2));
-quiv = quiver(rx,ry,u2d*scale,v2d*scale);
-quiv.Color = 'blue';
-quiv.LineWidth = 1.5;
-view(2);
-axis equal;
-grid minor;
