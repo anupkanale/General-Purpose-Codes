83a6e3ded9ec0947e678f4ed80d0ef87986d3834
anupvkanale <anupvkanale@gmail.com>
2017-08-08 16:36:55 -0700

    changed notation

--- ewaldSum_Stokes/getP.m
+++ ewaldSum_Stokes/getP.m
@@ -1,10 +1,10 @@
-function periodicVec = getP(v1,v2,v3,n,L)
+function periodicVec = getP(v1,v2,v3,n,box)
     periodicVec = zeros(3,(2*n+1)^3);
     index=1;
     for n1=-n:n
         for n2=-n:n
             for n3=-n:n
-                periodicVec(:,index) = n1*v1*L(1)+n2*v2*L(2) + n3*v3*L(3);
+                periodicVec(:,index) = n1*v1*box(1)+n2*v2*box(2) + n3*v3*box(3);
                 index=index+1;
             end
         end
