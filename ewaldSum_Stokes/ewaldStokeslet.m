dcd8d4ccd2926b877f82d9a6e98d12fcff987f3b
anupvkanale <anupvkanale@gmail.com>
2017-08-08 16:38:34 -0700

    corrected mistake from uSelf. Careful!

--- ewaldSum_Stokes/ewaldStokeslet.m
+++ ewaldSum_Stokes/ewaldStokeslet.m
@@ -3,14 +3,16 @@ clear; %close all
 tic
 
 % grid points
-res = 7;
+res = 51;
 nX = res;
-nY = res*2+1;
+nY = res;
 nZ = 1;
 nPoints = nX*nY*nZ;
-L = [2; 2; 2];
-rx = linspace(-1, 1, nX);
-ry = linspace(-1, 1, nY);
+L = 1;
+box = L*[1; 1; 1];
+rsize = L/2;
+rx = linspace(-rsize,rsize,nX);
+ry = linspace(-rsize,rsize,nY);
 rz = 0;
 rVec = zeros(3,nPoints);
 for kk=1:nZ
@@ -26,8 +28,8 @@ end
 nStokes = 2; % number of stokeslets
 fVec = zeros(3,nPoints);
 r_fVec = [-0.1 0.1; 0 0 ; 0 0];
-fVec(:,1) = 5*[-1; 0; 0];
-fVec(:,2) = 5*[1; 0; 0];
+fVec(:,1) = [-1; 0; 0];
+fVec(:,2) = [1; 0; 0];
 % fVec(:,3) = [1; 0; 0];
 % fVec(:,4) = [-1; 0; 0];
 
@@ -36,7 +38,7 @@ a1 = [1;0;0];
 a2 = [0;1;0];
 a3 = [0;0;1];
 nReal = 10;
-nImag = 10;
+nImag = 20;
 xi = 10;
 
 %% compute flowfield
@@ -44,12 +46,12 @@ velVec = zeros(3,nPoints);
 uReal  = zeros(3,nPoints);
 uFourier = zeros(3,nPoints);
 uSelf = zeros(3,nPoints);
-for mm=1:nPoints
+parfor mm=1:nPoints
     rVecM = rVec(:,mm);
     
-    uReal(:,mm) = realSum(nStokes,rVecM,r_fVec,fVec,xi,L,a1,a2,a3,nReal);
-    uFourier(:,mm) = fourierSum(nStokes,rVecM,r_fVec,fVec,xi,L,a1,a2,a3,nImag);
-    uSelf(:,mm) = 4*xi/sqrt(pi) * fVec(:,mm);
+    uReal(:,mm) = realSum(nStokes,rVecM,r_fVec,fVec,xi,box,a1,a2,a3,nReal);
+    uFourier(:,mm) = fourierSum(nStokes,rVecM,r_fVec,fVec,xi,box,a1,a2,a3,nImag);
+%     uSelf(:,mm) = 4*xi/sqrt(pi) * fVec(:,);
     
     velVec(:,mm) = uReal(:,mm) + uFourier(:,mm) - uSelf(:,mm);
 end
@@ -57,13 +59,6 @@ end
 
 %% Post-processing
 save('ewald_Stokes_data');
-
-% clf(figure(2))
-set(figure(), 'position', [1050 1050 850 700]);
-for ii=1:nStokes
-    plot(r_fVec(1,ii), r_fVec(2,ii), 'or','markersize', 10, 'markerfacecolor', 'r');
-    hold on;
-end
 
 % Convert to format appropriate for plotting
 u3d = zeros(nX,nY,nZ);
@@ -86,20 +81,29 @@ u2d = squeeze(u3d(:,:,(nZ+1)/2));
 v2d = squeeze(v3d(:,:,(nZ+1)/2));
 velMag2d = squeeze(velMag3d(:,:,(nZ+1)/2));
 
-[x,y] = meshgrid(rx,ry);
-% pcolor(x,y,velMag2d');
-% shading interp;
-% colorbar;
-% caxis([0 5]);
+%% Plots
+set(figure(), 'position', [1050 1050 850 700]);
 
+[x,y] = meshgrid(rx,ry);
+pcolor(x,y,velMag2d');
+shading interp;
+colorbar;
+caxis([0 5000]);
+
+hold on;
 quiv = quiver(rx,ry,u3d',v3d');
 quiv.Color = 'red';
 quiv.LineWidth = 1.5;
-% axis equal;
+axis equal;
 grid minor;
-xlim([-1.01 1.01]); ylim([-1.01 1.01]); % zlim([-0.5*L(3), 0.5*L(3)])
+xlim([-rsize rsize]); ylim([-rsize rsize]); % zlim([-0.5*L(3), 0.5*L(3)])
 % view(2);
 title('Ewald method');
+
+hold on;
+for ii=1:nStokes
+    plot(r_fVec(1,ii), r_fVec(2,ii), 'or','markersize', 10, 'markerfacecolor', 'r');
+end
 
 dummyvel = real(velVec');
 dummyr = rVec';
